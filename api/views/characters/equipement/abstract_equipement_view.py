6de72b48a5872d0d33a76ed1ebb7049c02c1fef6
Victor <victor.cyprien@limayrac.fr>
2023-09-18 11:06:54 +0200

    Refactorize code for searching equipement

--- api/views/characters/equipement/abstract_equipement_view.py
+++ api/views/characters/equipement/abstract_equipement_view.py
@@ -11,6 +11,29 @@ logger = logging.getLogger('console')
 
 class EquipementAbstractView(CharacterAbstractView):
 
+    def get_item_in_equipment(self, character_equipment: Dict, item_id: int, isVault: bool = False) -> Dict:
+        item_searched = {}
+        items = character_equipment["inventory"]["data"]["items"] if not isVault else \
+            character_equipment["profileInventory"]["data"]["items"]
+        for item in items:
+            # Check the hash (ID) of the weapon payload and equipment
+            item_instance_id = int(item.get("itemInstanceId", 0))
+            if item_instance_id == item_id:
+                # 0 : Can be tranfered
+                # 1 : You can't transfert because it's already equip
+                # 2 : Cannot be transfered
+                # 4 : There is no space remaining for the item you want to tranfert
+                if not item["transferStatus"] == 0:
+                    if item["transferStatus"] == 1:
+                        raise HTTPBadRequest(text="You can't transfert because it's already equip")
+                    if item["transferStatus"] == 2:
+                        raise HTTPBadRequest(text="This weapon cannot be transfered")
+                    if item["transferStatus"] == 4:
+                        raise HTTPBadRequest(text="There is no space remaining for the item you want to tranfert")
+                item_searched = item
+        return item_searched
+    
+
     async def get_equipment_object(
         self, 
         equipment_id: int,
@@ -42,43 +65,29 @@ class EquipementAbstractView(CharacterAbstractView):
         membership_type: int
     ) -> bool:
         weapon_transfered = False
-        for item in character_equipment["inventory"]["data"]["items"]:
-            # Check the hash (ID) of the weapon payload and equipment
-            item_instance_id = int(item.get("itemInstanceId", 0))
-            if item_instance_id == weapon_id:
-                # 0 : Can be tranfered
-                # 1 : You can't transfert because it's already equip
-                # 2 : Cannot be transfered
-                # 4 : There is no space remaining for the item you want to tranfert
-                if not item["transferStatus"] == 0:
-                    if item["transferStatus"] == 1:
-                        raise HTTPBadRequest(text="You can't transfert because it's already equip")
-                    if item["transferStatus"] == 2:
-                        raise HTTPBadRequest(text="This weapon cannot be transfered")
-                    if item["transferStatus"] == 4:
-                        raise HTTPBadRequest(text="There is no space remaining for the item you want to tranfert")
-                
-                async with self.bungie.client.acquire() as rest:
-                    try:
-                        await rest.transfer_item(
-                            access_token,
-                            item["itemInstanceId"],
-                            item["itemHash"],
-                            character_id_from,
-                            membership_type,
-                            vault=True
-                        )
-                        await rest.transfer_item(
-                            access_token,
-                            item["itemInstanceId"],
-                            item["itemHash"],
-                            character_id_to,
-                            membership_type,
-                        )
-                        weapon_transfered = True
-                    except HTTPError as error:
-                        logger.warning(error)
-                        raise HTTPNotFound(text="Something went wrong")
+        item = self.get_item_in_equipment(character_equipment, weapon_id)
+        if item:
+            async with self.bungie.client.acquire() as rest:
+                try:
+                    await rest.transfer_item(
+                        access_token,
+                        item["itemInstanceId"],
+                        item["itemHash"],
+                        character_id_from,
+                        membership_type,
+                        vault=True
+                    )
+                    await rest.transfer_item(
+                        access_token,
+                        item["itemInstanceId"],
+                        item["itemHash"],
+                        character_id_to,
+                        membership_type,
+                    )
+                    weapon_transfered = True
+                except HTTPError as error:
+                    logger.warning(error)
+                    raise HTTPNotFound(text="Something went wrong")
         return weapon_transfered
 
 
@@ -91,24 +100,22 @@ class EquipementAbstractView(CharacterAbstractView):
         membership_type: int
     ) -> bool:
         weapon_equipped = False
-        for item in character_equipment["inventory"]["data"]["items"]:
-            # Check the hash (ID) of the weapon payload and equipment
-            item_instance_id = int(item.get("itemInstanceId", 0))
-            if item_instance_id == weapon_id:
-                async with self.bungie.client.acquire() as rest:
-                    try:
-                        await rest.equip_item(
-                            access_token,
-                            item["itemInstanceId"],
-                            character_id,
-                            membership_type
-                        )
-                        weapon_equipped = True
-                    except InternalServerError as error:
-                        if error.error_status == "DestinyItemUniqueEquipRestricted":
-                            raise HTTPBadRequest(text="You can't equip more than two exotic weapon")
-                        if error.error_status == "DestinyItemNotFound":
-                            raise HTTPNotFound(text="This item doesn't exist")
+        item = self.get_item_in_equipment(character_equipment, weapon_id)
+        if item:
+            async with self.bungie.client.acquire() as rest:
+                try:
+                    await rest.equip_item(
+                        access_token,
+                        item["itemInstanceId"],
+                        character_id,
+                        membership_type
+                    )
+                    weapon_equipped = True
+                except InternalServerError as error:
+                    if error.error_status == "DestinyItemUniqueEquipRestricted":
+                        raise HTTPBadRequest(text="You can't equip more than two exotic weapon")
+                    if error.error_status == "DestinyItemNotFound":
+                        raise HTTPNotFound(text="This item doesn't exist")
         return weapon_equipped
 
 
@@ -121,25 +128,23 @@ class EquipementAbstractView(CharacterAbstractView):
         membership_type: int
     ):
         weapon_stored = False
-        for item in character_equipment["inventory"]["data"]["items"]:
-            # Check the hash (ID) of the weapon payload and equipment
-            item_instance_id = int(item.get("itemInstanceId", 0))
-            if item_instance_id == weapon_id:
-                async with self.bungie.client.acquire() as rest:
-                    try:
-                        await rest.transfer_item(
-                            access_token,
-                            item["itemInstanceId"],
-                            item["itemHash"],
-                            character_id,
-                            membership_type,
-                            vault=True
-                        )
-                        weapon_stored = True
-                    except InternalServerError as error:
-                        logger.exception(error)
-                        if error.error_status == "DestinyItemNotFound":
-                            raise HTTPNotFound(text="This item doesn't exist")
+        item = self.get_item_in_equipment(character_equipment, weapon_id)
+        if item:
+            async with self.bungie.client.acquire() as rest:
+                try:
+                    await rest.transfer_item(
+                        access_token,
+                        item["itemInstanceId"],
+                        item["itemHash"],
+                        character_id,
+                        membership_type,
+                        vault=True
+                    )
+                    weapon_stored = True
+                except InternalServerError as error:
+                    logger.exception(error)
+                    if error.error_status == "DestinyItemNotFound":
+                        raise HTTPNotFound(text="This item doesn't exist")
         return weapon_stored
     
 
@@ -162,21 +167,20 @@ class EquipementAbstractView(CharacterAbstractView):
                 access_token
             )
         
-        for item in vault_content["profileInventory"]["data"]["items"]:
-            item_instance_id = int(item.get("itemInstanceId", 0))
-            if item_instance_id == weapon_id:
-                async with self.bungie.client.acquire() as rest:
-                    try:
-                        await rest.transfer_item(
-                            access_token,
-                            item["itemInstanceId"],
-                            item["itemHash"],
-                            character_id,
-                            membership_type,
-                        )
-                        weapon_retrieved = True
-                    except HTTPError as error:
-                        logger.warning(error)
-                        raise HTTPNotFound(text="Something went wrong")
+        item = self.get_item_in_equipment(vault_content, weapon_id, isVault=True)
+        if item:
+            async with self.bungie.client.acquire() as rest:
+                try:
+                    await rest.transfer_item(
+                        access_token,
+                        item["itemInstanceId"],
+                        item["itemHash"],
+                        character_id,
+                        membership_type,
+                    )
+                    weapon_retrieved = True
+                except HTTPError as error:
+                    logger.warning(error)
+                    raise HTTPNotFound(text="Something went wrong")
 
         return weapon_retrieved
