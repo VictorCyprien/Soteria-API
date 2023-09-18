4d99c34f4d59e9def33ce42f9e04667b78395e7a
Victor <victor.cyprien@limayrac.fr>
2023-09-18 10:14:51 +0200

    Update payload schema

--- api/schemas/weapon_infos_schema.py
+++ api/schemas/weapon_infos_schema.py
@@ -2,7 +2,7 @@ from marshmallow import Schema, fields
 
 
 class WeaponPayloadSchema(Schema):
-    weapon_id = fields.Integer(
+    character_id_pull_equipment = fields.Integer(
         required=True,
-        metadata={"description": "The ID of the weapon"}
+        metadata={"description": "The ID of the character we want to get the weapon"}
     )
