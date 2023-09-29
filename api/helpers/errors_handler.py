c5666264939c05f48e33d8d0bd11a130d4e6aab6
Victor <victor.cyprien@limayrac.fr>
2023-09-29 09:46:54 +0200

    Update `ReasonError` enum

--- api/helpers/errors_handler.py
+++ api/helpers/errors_handler.py
@@ -24,6 +24,20 @@ class Unauthorized(HTTPUnauthorized):
 
 
 class ReasonError(Enum):
-    # TODO : Add error here...
     PROFILE_NOT_FOUND = "Profile not found"
     NOT_AUTHENTICATED = "Not Authenticated"
+    CHARACTER_NOT_FOUND = "Character not found"
+    ITEM_NOT_FOUND = "This item doesn't exist"
+    ITEM_NOT_IN_CHARACTER = "The item is not on this character"
+    ITEM_NOT_FOUND_IN_INVENTORY = "Item not found in the inventory"
+    EQUIPMENT_CANNOT_BE_TRANSFERED = "This equipment cannot be transfered"
+    EQUIPMENT_ALREADY_EQUIPED = "You can't transfert because it's already equip"
+    NO_SPACE_REMAINING = "There is no space remaining for the item you want to tranfert"
+    BUNGIE_API_ERROR = "Something went wrong, please try again"
+    EQUIP_TWO_EXOTIC = "You can't equip more than two exotic weapon or two exotic armor"
+    ITEM_ALREADY_EQUIPPED = "This item is already equipped or is not in the equipment"
+    ITEM_NOT_IN_VAULT = "The item is not on in the Vault"
+    ITEM_CANNOT_BE_STORED_IN_VAULT = "The item is not on this character or is equiped"
+    MANIFEST_NOT_FOUND = "Manifest not found"
+    REDIRECT_ERROR = "Code not found, aborting..."
+    VENDOR_NOT_FOUND = "Vendor not found"
